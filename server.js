317609f9963349a33faeece0c16c0f1c5f0dbd00
Vivek Ramesh <Vivek Ramesh>
2016-10-03 00:49:07 +0530

    removed express-status-monitor module

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ const path = require('path');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const expressValidator = require('express-validator');
-const expressStatusMonitor = require('express-status-monitor');
 const sass = require('node-sass-middleware');
 const multer = require('multer');
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
@@ -81,7 +80,6 @@ function exitDatabase() {
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.use(expressStatusMonitor());
 app.use(compression());
 app.use(sass({
   src: path.join(__dirname, 'public'),
