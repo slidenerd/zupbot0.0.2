c28efe329e0a7b0e7a5d62634b5e8ebdd2079d4c
Vivek Ramesh <Vivek Ramesh>
2016-10-13 18:18:08 +0530

    testing facebook messenger cab

--- server.js
+++ server.js
@@ -468,6 +468,7 @@ function handleSpecialReplies(session, response) {
     let destination = brain.get(session.message.user.id, 'cabdestination')
     let url = encodeURI('https://zup.chat/api/ride?lat=' + latitude + '&long=' + longitude + '&drop=' + destination);
     platforms.getWebViewButton(session, response.data, url, 'Your Cab', 'tall');
+    brain.set(session.message.user.id, 'topic', 'random');
   }
   else {
     session.send(response);
