874ad372ac8983c18faed8722cece2f3ad9cc334
Vivek Ramesh <Vivek Ramesh>
2016-10-15 07:28:56 +0530

    testing quick reply to ask ola or uber

--- bot/rive.js
+++ bot/rive.js
@@ -23,6 +23,7 @@ let brain = {
 brain.initSubroutines = function () {
     brain.riveScript.setSubroutine(all.flipkart.name, all.flipkart.subroutine)
     brain.riveScript.setSubroutine(all.ride.name, all.ride.subroutine)
+    brain.riveScript.setSubroutine(all.cabProvider.name, all.cabProvider.subroutine)
     brain.riveScript.setSubroutine(all.weather.name, all.weather.subroutine)
     brain.riveScript.setSubroutine(all.location.name, all.location.subroutine)
 }
