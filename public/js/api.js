a3d1147a1343717718b4206c99670c14ba6f7d28
Vivek Ramesh <Vivek Ramesh>
2016-10-19 19:36:18 +0530

    testing prabhu s changes to api.js under public folder to fix email and contact form issue

--- public/js/api.js
+++ public/js/api.js
@@ -3,91 +3,74 @@ var baseURL = "https://zup.chat/api/", call_url = "";
 // Email Subscribe
 function subscribeEmail(email) {
     call_url = "subscribe";
-    var successMsg = 'Subscribed successfully, Please check your mail, thanks.';
-    var errorMsg = 'Subscribe failed.';
-    var form = new FormData();
-    form.append( 'email', email );
+    var form = new Object();
+    form['email'] = email;
+
     var settings = {
+      "async": true,
+      "crossDomain": true,
       "url": baseURL+call_url,
-      "type": "POST",
+      "method": "POST",
+      "crossOrigin" :true,
       "xhrFields": { withCredentials: true },
       "processData": false,
-      "mimeType": "multipart/form-data",
-      "data": form
+      "contentType": false,
+      "data": JSON.stringify(form)
     }
 
     $.ajax(settings).success(function (response) {
         console.log(response);
         if(response.success == true) {
-            fakeMessage(successMsg);
+            fakeMessage(2);
         } else {
-            fakeMessage(errorMsg);
+            fakeMessage(3);
         }
     }).error(function (errorResponse) {
         console.warn(errorResponse);
-        fakeMessage(errorMsg);
+        fakeMessage(3);
     });
 }
 
 // Contact Us
 function contactUs() {
-    call_url = "sendMail";
-    var successMsg = "Your request has been sent, we will reach out to you shortly.";
-    var errorMsg = "Your request has been not sent, please try again later.";
+    call_url = "sendMail"
     var type = '';
-    var form = new FormData();
-    var opts = {
-        "closeButton": true,
-        "debug": false,
-        "positionClass": "toast-top-right",
-        "onclick": null,
-        "showDuration": "300",
-        "hideDuration": "1000",
-        "timeOut": "5000",
-        "extendedTimeOut": "1000",
-        "showEasing": "swing",
-        "hideEasing": "linear",
-        "showMethod": "fadeIn",
-        "hideMethod": "fadeOut"
-    };
-    
-    $('body').append('<div class="loader-bg"><div class="loader"></div></div>');
+    var form = new Object();
+    form['name'] = $('#form1').val();
+    form['email'] = $('#form1').val();
+    form['subject'] = $('#form1').val();
+    form['message'] = $('#form1').val();
+    form['location'] = $('#form1').val();
+    form['name'] = $('#form1').val();
 
-    form.append( 'name', $('#form1').val() );
-    form.append( 'email', $('#form2').val() );
-    form.append( 'subject', $('#form3').val() );
-    form.append( 'message', $('#form4').val() );
-    form.append( 'location', $('#form5').val() );
     if($('#form6').val() == 1) {
         type = 'individual';
     } else if($('#form6').val() == 1) {
         type = 'company';
     }
-    form.append( 'type',  type);
+    form['type'] = $('#form1').val();
 
     var settings = {
+      "async": true,
+      "crossDomain": true,
       "url": baseURL+call_url,
-      "type": "POST",
+      "method": "POST",
       "crossOrigin" :true,
       "xhrFields": { withCredentials: true },
       "processData": false,
-      "mimeType": "multipart/form-data",
-      "data": form
+      "contentType": false,
+      "dataType": "json",
+      "data": JSON.stringify(form)
     }
 
     $.ajax(settings).success(function (response) {
         console.log(response);
         if(response.success == true) {
             clear();
-            toastr.success(successMsg, null, opts);
-        } else {
-            toastr.error(errorMsg, null, opts);
         }
     }).error(function (errorResponse) {
         console.warn(errorResponse);
-        toastr.error(errorMsg, null, opts);
     }).complete(function(completeResponse) {
-        $('.loader-bg').remove();
         if(completeResponse.success == true) {
             clear();
         }
